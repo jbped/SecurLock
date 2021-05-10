2ebbbc582d665fa8a59522c6d960d8c7295bc0f6
jbped <pedigojacob@gmail.com>
2021-05-09 23:48:07 -0600

    Created arrays for each alphanumeric option

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function writePassword() {
 
 }
 
+lowercaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+uppercaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','R','X','Y','Z'];
+numbersArray = ['0','1','2','3','4','5','6','7','8','9'];
+specialCharactersArray = [" ", "!", "#", "$", "%", "&", "'",'"', "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+var generatePassword = function () {
+  // choiceArray = userChoices();
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
